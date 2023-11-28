41fbdb9b81046767708581f826e1f4023bb0ea9a
AntoineD <>
2023-11-28 03:05:47 +0000

    Update dependencies

--- src/gemseo_mma/opt/__init__.py
+++ src/gemseo_mma/opt/__init__.py
@@ -13,4 +13,5 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Options for Bi-Level Outer Approximation."""
+
 from __future__ import annotations
